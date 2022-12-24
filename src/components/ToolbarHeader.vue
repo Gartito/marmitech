539b7604bded75f012670a847a6b1c44ff8d9c88
Gabriel Pacheco <gab.pacheco1998@gmail.com>
2022-12-24 00:42:29 -0300

    Header bg-blue

--- src/components/ToolbarHeader.vue
+++ src/components/ToolbarHeader.vue
@@ -1,19 +1,13 @@
 <template>
   <div>
-    <q-toolbar class="bg-red-10 text-white q-my-md shadow-2" id="toolbarHeader">
+    <q-toolbar class="bg-blue-10 text-white q-my-md shadow-2" id="toolbarHeader">
       <p class="text-h2 text-weight-bolder">MARMITECH</p>
       <p class="text-weight-bold text-h5 q-pl-lg q-mt-lg">
         Livorno Restaurante
       </p>
       <q-space />
 
-      <q-btn-toggle
-        v-model="model"
-        flat
-        stretch
-        toggle-color="yellow"
-        :options="options"
-      />
+      <q-btn-toggle v-model="model" flat stretch toggle-color="yellow" :options="options" />
     </q-toolbar>
   </div>
 </template>
