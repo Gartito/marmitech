737264d624b00a1ae648506b1f912c0d713511e8
Gabriel Pacheco <91842180+Gartito@users.noreply.github.com>
2022-12-24 02:28:39 -0300

    Update Menus_grid.vue

--- src/components/Menus_grid.vue
+++ src/components/Menus_grid.vue
@@ -64,7 +64,7 @@ export default {
       for (var i = 0; i < this.rows.length; i++) {
         this.rows[i].menu_of_day = this.rows[i].menu_of_day.join(", ");
       }
-      alert(process.env.TESTE)
+      alert(process.env.ENV_VAR_TESTE)
     },
   },
   created() {
